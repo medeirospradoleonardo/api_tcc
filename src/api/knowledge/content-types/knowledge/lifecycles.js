32f2be8b37a8e2b53b669e8e931196706a7ce95a
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-09-30 15:18:49 -0300

    att

--- src/api/knowledge/content-types/knowledge/lifecycles.js
+++ src/api/knowledge/content-types/knowledge/lifecycles.js
@@ -37,18 +37,39 @@ module.exports = {
     const ctx = strapi.requestContext.get();
     const { user } = ctx.state;
 
-    const { stories } = await strapi.entityService.findOne('api::knowledge.knowledge', id, {
+    const { stories, categories } = await strapi.entityService.findOne('api::knowledge.knowledge', id, {
       populate: '*',
     })
     
+    // criando o story
     const entry = await strapi.entityService.create('api::story.story', {
       data: {
         author: user.username ? user.username : 'admin',
         date: new Date().toISOString() 
       },
     });
-
+    
+    // colocando o story
     data.stories = stories.concat([entry.id])
+
+
+    // verificando as categorias
+    let categoriesIdToVerify = []
+    if(data.categories.disconnect){
+      categoriesIdToVerify = data.categories.disconnect.map((c) => c.id)
+    }else{
+      const oldCategories = categories.map((c) => c.id)
+      categoriesIdToVerify = oldCategories.filter(c => !data.categories.map((c) => parseInt(c)).includes(c))
+    }
+    
+    categoriesIdToVerify.map(async (c) => {
+      const { knowledges } = await strapi.entityService.findOne('api::category.category', c, {
+        populate: '*',
+      })
+
+      knowledges.length == 1 && await strapi.entityService.delete('api::category.category', c);
+    })
+
   },
   async beforeDelete(event) {
     const { where, data } = event.params;
@@ -61,6 +82,18 @@ module.exports = {
 
     stories.map(async (h) => {
       await strapi.entityService.delete('api::story.story', h.id);
+    })
+
+    // deletando todos as categorias que so estiverem nesse knowledge
+    const categories = await strapi.entityService.findMany('api::category.category',{
+      filters: { 
+        knowledges: [id]
+      },
+      populate: '*',
+    })
+
+    categories.map(async (c) => {
+      c.knowledges.length == 1 && await strapi.entityService.delete('api::category.category', c.id);
     })
   }
 };
